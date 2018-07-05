eefa3be1e506d57b41496f47e19cd79183cc0531
Ahmed Hesham <eng.ahmed.86313@gmail.com>
2018-07-05 13:11:09 +0200

    Loader for DetailsActivity

--- app/src/main/java/com/example/android/movies/MovieDetailsActivity.java
+++ app/src/main/java/com/example/android/movies/MovieDetailsActivity.java
@@ -3,6 +3,11 @@ package com.example.android.movies;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
@@ -21,7 +26,7 @@ import java.net.URL;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MovieDetailsActivity extends AppCompatActivity {
+public class MovieDetailsActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Movie>{
 
     @BindView(R.id.iv_poster_movie)
     ImageView mPosterMovieImageView;
@@ -41,6 +46,8 @@ public class MovieDetailsActivity extends AppCompatActivity {
     TextView mDetailErrorMessageTextView;
 
     private static String API_KEY_VALUE;
+    private static final String Movie_ID = "movie_id";
+    private static final int MOVIE_LOADER_ID = 52;
 
 
     @Override
@@ -58,7 +65,16 @@ public class MovieDetailsActivity extends AppCompatActivity {
 
         if (intent.hasExtra(Intent.EXTRA_TEXT)) {
             String movieId = intent.getStringExtra(Intent.EXTRA_TEXT);
-            new FetchMovieData().execute(movieId);
+            Bundle movieBundle = new Bundle();
+            movieBundle.putString(Movie_ID, movieId);
+
+            LoaderManager loaderManager = getSupportLoaderManager();
+            Loader<Movie> movieLoader = loaderManager.getLoader(MOVIE_LOADER_ID);
+            if (movieLoader == null){
+                loaderManager.initLoader(MOVIE_LOADER_ID, movieBundle, this);
+            }else {
+                loaderManager.restartLoader(MOVIE_LOADER_ID, movieBundle, this);
+            }
         } else {
             closeActivity();
         }
@@ -69,51 +85,65 @@ public class MovieDetailsActivity extends AppCompatActivity {
         Toast.makeText(this, getResources().getString(R.string.error_message), Toast.LENGTH_SHORT).show();
     }
 
-    public class FetchMovieData extends AsyncTask<String, Void, Movie> {
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            mMovieProgressBar.setVisibility(View.VISIBLE);
-            mMovieDataLinearLayout.setVisibility(View.GONE);
-        }
-
-        @Override
-        protected Movie doInBackground(String... strings) {
-            String idValue = strings[0];
-            URL moviesRequestURL = NetworkUtils.buildUrl(API_KEY_VALUE, idValue);
-            try {
-                String jsonMovieData = NetworkUtils.getResponseFromHttpUrl(moviesRequestURL);
-                return JsonUtils.parseMovieDataFromJson(jsonMovieData);
-            } catch (Exception e) {
-                e.printStackTrace();
-                return null;
+    @NonNull
+    @Override
+    public Loader<Movie> onCreateLoader(int id, @Nullable final Bundle args) {
+        return new AsyncTaskLoader<Movie>(this) {
+            @Override
+            protected void onStartLoading() {
+                if (args == null || !args.containsKey(Movie_ID)) {
+                    closeActivity();
+                    return;
+                }
+                mMovieProgressBar.setVisibility(View.VISIBLE);
+                mMovieDataLinearLayout.setVisibility(View.GONE);
+                forceLoad();
             }
-        }
 
-        @Override
-        protected void onPostExecute(Movie movieData) {
-            if (movieData != null) {
-                showMovieDetailData(movieData);
-            } else {
-                showDetailErrorMessage();
+            @Nullable
+            @Override
+            public Movie loadInBackground() {
+                String idValue = args.getString(Movie_ID);
+                URL moviesRequestURL = NetworkUtils.buildUrl(API_KEY_VALUE, idValue);
+                try {
+                    String jsonMovieData = NetworkUtils.getResponseFromHttpUrl(moviesRequestURL);
+                    return JsonUtils.parseMovieDataFromJson(jsonMovieData);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    return null;
+                }
             }
-        }
+        };
+    }
 
-        private void showMovieDetailData(Movie movieData) {
-            mMovieProgressBar.setVisibility(View.GONE);
-            mMovieDataLinearLayout.setVisibility(View.VISIBLE);
-            Picasso.get().load("http://image.tmdb.org/t/p/w500//" + movieData.getPosterPath()).into(mPosterMovieImageView);
-            mOriginalTitleTextView.setText(movieData.getOriginalTitle());
-            mOverviewTextView.setText(movieData.getOverView());
-            mVoteAverageTextView.setText(movieData.getVoteAverage());
-            mReleaseDateTextView.setText(movieData.getReleaseDate());
+    @Override
+    public void onLoadFinished(@NonNull Loader<Movie> loader, Movie movieData) {
+        if (movieData != null) {
+            showMovieDetailData(movieData);
+        } else {
+            showDetailErrorMessage();
         }
+    }
 
-        private void showDetailErrorMessage() {
-            mPosterMovieImageView.setVisibility(View.GONE);
-            mMovieProgressBar.setVisibility(View.GONE);
-            mMovieDataLinearLayout.setVisibility(View.GONE);
-            mDetailErrorMessageTextView.setVisibility(View.VISIBLE);
-        }
+    @Override
+    public void onLoaderReset(@NonNull Loader<Movie> loader) {
+
+    }
+
+    private void showMovieDetailData(Movie movieData) {
+        mMovieProgressBar.setVisibility(View.GONE);
+        mMovieDataLinearLayout.setVisibility(View.VISIBLE);
+        Picasso.get().load("http://image.tmdb.org/t/p/w500//" + movieData.getPosterPath()).into(mPosterMovieImageView);
+        mOriginalTitleTextView.setText(movieData.getOriginalTitle());
+        mOverviewTextView.setText(movieData.getOverView());
+        mVoteAverageTextView.setText(movieData.getVoteAverage());
+        mReleaseDateTextView.setText(movieData.getReleaseDate());
+    }
+
+    private void showDetailErrorMessage() {
+        mPosterMovieImageView.setVisibility(View.GONE);
+        mMovieProgressBar.setVisibility(View.GONE);
+        mMovieDataLinearLayout.setVisibility(View.GONE);
+        mDetailErrorMessageTextView.setVisibility(View.VISIBLE);
     }
 }
